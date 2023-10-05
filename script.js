cafe88584c4869adf5e42e53d05a2c266d8ce095
Sumit Raj Singh <133523998+krishnansumitsingh8808@users.noreply.github.com>
2023-10-05 14:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getFormvalue() {
     //Write your code here
 	let result=document.querySelectorAll("input");
-	let temp=result[0].innerHTML;
-	let temp1=result[1].innerHTML;
+	let temp=result[0].value;
+	let temp1=result[1].value;
 	let mainresult=`${temp} ${temp1}`
 	return alert(mainresult);
 	
