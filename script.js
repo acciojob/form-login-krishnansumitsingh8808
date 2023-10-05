db20a30be08c4ea15b8dddb3066f67c0a0a95724
Sumit Raj Singh <133523998+krishnansumitsingh8808@users.noreply.github.com>
2023-10-05 14:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let result=document.getElementByTagName("input");
+	let temp=result[0].innerHTML;
+	let temp1=result[1].innerHTML;
+	let mainresult=`${temp} ${temp1}`
+	return alert(mainresult);
+	
 
 }
